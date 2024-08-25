ee34b9f257cf699612a683245a05224286e384cf
liukangtian <liukangtian@topnet.net.cn>
2024-08-25 22:19:51 +0800

    first commit

---
+++ src/components/AppProviders/AppProviders.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container>
+    <el-header>
+      <Header></Header>
+    </el-header>
+    <el-main>
+      <page></page>
+    </el-main>
+    <el-footer>
+      <Footer></Footer>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+import Header from './components/Header/Header.vue';
+import Page from './components/Page/Page.vue';
+import Footer from './components/Footer/Footer.vue';
+
+export default {
+  name: 'AppProviders',
+  components: { Header, Page, Footer },
+  data() {
+    return {
+      defaultTheme: 'dark',
+      initialIsOpen: false,
+    };
+  },
+};
+</script>
